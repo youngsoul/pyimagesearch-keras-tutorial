ebcd902955daf79e73e52684418edacb7f772704
patrickryan <theyoungsoul@gmail.com>
2019-01-16 12:15:49 -0500

    initial

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ln -s /usr/local/Cellar/opencv/4.0.1/lib/python3.7/site-packages/cv2/python-3.7/cv2.cpython-37m-darwin.so venv/lib/python3.6/site-packages/cv2.so
+pip install numpy
+pip install scikit-learn
+pip install tensorflow
+pip install keras
+pip install imutils
+pip install matplotlib
+
